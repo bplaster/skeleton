2a0d95fca37de84fa12d7a8f21970905e97f4c53
alapparikh <alap18@gmail.com>
2015-05-02 22:33:22 -0400

    rotation

--- src/light.cpp
+++ src/light.cpp
@@ -259,7 +259,7 @@ mat4f lighthdl::convert_to_matrix (GLfloat arr[16]) {
     
     for (int i=0; i<4; i++){
         for (int j=0; j<4; j++){
-            matrix[j][i] = arr[i+j];
+            matrix[i][j] = arr[4*i+j];
         }
     }
     
