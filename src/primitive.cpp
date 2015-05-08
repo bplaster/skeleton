113bb3eef6c4fcd9ee5fece0d09cbb0006d6532d
alapparikh <alap18@gmail.com>
2015-05-08 17:37:54 -0400

    working on sphere

--- src/primitive.cpp
+++ src/primitive.cpp
@@ -113,7 +113,7 @@ spherehdl::spherehdl(float radius, int levels, int slices)
 	rigid[0].geometry.reserve(2 + (levels-1)*slices);
 	rigid[0].geometry.push_back(vec8f(0.0, 0.0, radius, 0.0, 0.0, 1.0, textureS(0.0, 1.0), textureT(0.0)));
 	for (int i = 1; i < levels; i++)
-		for (int j = 0; j < slices; j++)
+		for (int j = 0; j <= slices; j++)
 		{
 			vec3f dir(sin(m_pi*(float)i/(float)levels)*cos(2.0*m_pi*(float)j/(float)slices),
 					  sin(m_pi*(float)i/(float)levels)*sin(2.0*m_pi*(float)j/(float)slices),
@@ -125,7 +125,7 @@ spherehdl::spherehdl(float radius, int levels, int slices)
 
 	for (int i = 0; i < slices; i++)
 	{
-		rigid[0].indices.push_back(1 + (i+1)%slices);
+		rigid[0].indices.push_back(1 + (i+1));
 		rigid[0].indices.push_back(1 + i);
 		rigid[0].indices.push_back(0);
 	}
@@ -133,20 +133,20 @@ spherehdl::spherehdl(float radius, int levels, int slices)
 	for (int i = 0; i < levels-2; i++)
 		for (int j = 0; j < slices; j++)
 		{
-			rigid[0].indices.push_back(1 + i*slices + j);
-			rigid[0].indices.push_back(1 + i*slices + (j+1)%slices);
-			rigid[0].indices.push_back(1 + (i+1)*slices + j);
+			rigid[0].indices.push_back(1 + i*(slices+1) + j);
+			rigid[0].indices.push_back(1 + i*(slices+1) + (j+1));
+			rigid[0].indices.push_back(1 + (i+1)*(slices+1) + j);
 
-			rigid[0].indices.push_back(1 + (i+1)*slices + j);
-			rigid[0].indices.push_back(1 + i*slices + (j+1)%slices);
-			rigid[0].indices.push_back(1 + (i+1)*slices + (j+1)%slices);
+			rigid[0].indices.push_back(1 + (i+1)*(slices+1) + j);
+			rigid[0].indices.push_back(1 + i*(slices+1) + (j+1));
+			rigid[0].indices.push_back(1 + (i+1)*(slices+1) + (j+1));
 		}
 
 	for (int i = 0; i < slices; i++)
 	{
-		rigid[0].indices.push_back(1 + (levels-1)*slices);
-		rigid[0].indices.push_back(1 + (levels-2)*slices + i);
-		rigid[0].indices.push_back(1 + (levels-2)*slices + (i+1)%slices);
+		rigid[0].indices.push_back(1 + (levels-1)*(slices+1));
+		rigid[0].indices.push_back(1 + (levels-2)*(slices+1) + i);
+		rigid[0].indices.push_back(1 + (levels-2)*(slices+1) + (i+1));
 	}
 
 	bound = vec6f(-radius, radius, -radius, radius, -radius, radius);
